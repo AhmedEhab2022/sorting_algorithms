893f089fe0799e11f4e105afdca82d9e245e194f
mohammed shousha <mshousha29@gmail.com>
2023-09-21 10:10:31 -0700

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,28 +8,28 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_idx;
-    int tmp;
+	size_t i, j, min_idx;
+	int tmp;
 
-    if (array == NULL)
-        return;
+	if (array == NULL)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min_idx = i;
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
 
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min_idx])
-                min_idx = j;
-        }
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
 
-        if (i != min_idx)
-        {
-            tmp = array[i];
-            array[i] = array[min_idx];
-            array[min_idx] = tmp;
-            print_array(array, size);
-        }
-    }
+		if (i != min_idx)
+		{
+			tmp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = tmp;
+			print_array(array, size);
+		}
+	}
 }
