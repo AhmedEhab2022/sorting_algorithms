dc23e979f2c6d539066803cd198313e0e0b9fece
root <am8122004@gmail.com>
2023-09-21 13:26:29 -0700

    insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,7 +10,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *curr, *prev, *tmp;
 
-	if (*list == NULL || (*list)->next == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
 	curr = (*list)->next;
