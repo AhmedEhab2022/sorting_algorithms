4056b55c5f4fa6f85336ebcfe4769f7014d6b5a4
mohammed shousha <mshousha29@gmail.com>
2023-09-21 10:02:38 -0700

    2-selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sort list with selection_sort
+ *
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
+ */
+void selection_sort(int *array, size_t size)
+{
+    size_t i, j, min_idx;
+    int tmp;
+
+    if (array == NULL)
+        return;
+
+    for (i = 0; i < size - 1; i++)
+    {
+        min_idx = i;
+
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[min_idx])
+                min_idx = j;
+        }
+
+        if (i != min_idx)
+        {
+            tmp = array[i];
+            array[i] = array[min_idx];
+            array[min_idx] = tmp;
+            print_array(array, size);
+        }
+    }
+}
