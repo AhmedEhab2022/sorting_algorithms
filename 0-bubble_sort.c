434e573016cd7b14e2b7ee21863eaca96f604494
mohammed shousha <mshousha29@gmail.com>
2023-09-21 09:42:23 -0700

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sort list with bubble
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t n = 0, len = size;
+	int tmp;
+
+	if (array == NULL)
+		return;
+
+	for (len = size; len > 0; len--)
+	{
+		for (n = 0; n < size - 1; n++)
+		{
+			if (array[n] > array[n + 1])
+			{
+				tmp = array[n];
+				array[n] = array[n + 1];
+				array[n + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+}
